4a1218fced1414629d22cd401456182f934adf3a
sudipta <sudipta.chtt@gmail.com>
2024-09-23 17:13:34 +0100

    as the file code somehow gone missing in the first commit

--- src/test/java/org/example/pages/CarDetailsPage.java
+++ src/test/java/org/example/pages/CarDetailsPage.java
@@ -1,2 +1,45 @@
-package org.example.pages;public class CarDetailsPage {
+package org.example.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class CarDetailsPage {
+
+    public WebElement getInputElement(WebDriver driver) {
+
+        WebElement inputElement = driver.findElement(By.id("registration-number-input"));
+        return inputElement;
+    }
+
+    public WebElement getFindButton(WebDriver driver) {
+
+        WebElement findElement = driver.findElement(By.id("find-vehicle-btn"));
+        return findElement;
+    }
+
+    public WebElement getRegNoElement(WebDriver driver) {
+
+        WebElement regNoElement = driver.findElement(By.xpath("/html[1]/body[1]/main[1]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/p[1]/b[1]"));
+        return regNoElement;
+    }
+
+
+    public WebElement getMakeElement(WebDriver driver) {
+
+        WebElement makeElement = driver.findElement(By.xpath("//html[1]/body[1]/main[1]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/p[2]/b[1]"));
+        return makeElement;
+    }
+    
+    public WebElement changeVehicleElement(WebDriver driver) {
+        WebElement changeVehElement = driver.findElement(By.id("change-vehicle-btn"));
+        return changeVehElement;
+    }
+
+    public WebElement getYearElement(WebDriver driver) {
+        WebElement yearElement = driver.findElement(By.xpath("/html[1]/body[1]/main[1]/form[1]/div[2]/div[1]/div[2]/div[1]/div[1]/p[5]/b[1]"));
+        return yearElement;
+    }
+
 }
+
--- src/test/java/org/example/utils/CarRegDetails.java
+++ src/test/java/org/example/utils/CarRegDetails.java
@@ -1,2 +1,42 @@
-package org.example.utils;public class CarRegDetails {
+package org.example.utils;
+
+public class CarRegDetails {
+
+    private final String regNo;
+    private final String make;
+    private final String model;
+    private final String year;
+
+    public CarRegDetails(String regNo, String make, String model, String year) {
+        this.regNo = regNo;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return "CarRegDetails{" +
+                "regNo='" + regNo + '\'' +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year='" + year + '\'' +
+                '}';
+    }
 }
